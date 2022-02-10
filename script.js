d9373ae65b62118718ac7ae004dd4102bf5604a9
Protocol <yazan.bakour@ipricegroup.com>
2022-02-10 17:53:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,268 @@
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 1013 - margin.left - margin.right,
+    height = 431 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#vis").append('div').attr('id', 'chart').style('display', 'inline-block').style('margin-right', '40px')
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+var data = [
+  {
+    "Subject": "Preparation of interview(s)",
+    "Ratings": "129",
+    "Priority Score": "54.26312316788588",
+    "ABS Correlation Coefficient (NPS)": "0.60",
+    "Average Rating": "6.4",
+  },
+  {
+    "Subject": "Proficiency interviewer",
+    "Ratings": "124",
+    "Priority Score": "33.86156756442392",
+    "ABS Correlation Coefficient (NPS)": "0.66",
+    "Average Rating": "7.4",
+  },
+  {
+    "Subject": "Proficiency recruiter",
+    "Ratings": "123",
+    "Priority Score": "27.126708598970215",
+    "ABS Correlation Coefficient (NPS)": "0.61",
+    "Average Rating": "7.5",
+  },
+  {
+    "Subject": "Quick response",
+    "Ratings": "117",
+    "Priority Score": "17.398356024544675",
+    "ABS Correlation Coefficient (NPS)": "0.37",
+    "Average Rating": "7.0",
+  },
+  {
+    "Subject": "Speed of the process",
+    "Ratings": "95",
+    "Priority Score": "13.663879946546723",
+    "ABS Correlation Coefficient (NPS)": "0.27",
+    "Average Rating": "6.6",
+  },
+  {
+    "Subject": "Planning of interview(s)",
+    "Ratings": "95",
+    "Priority Score": "11.427383854772343",
+    "ABS Correlation Coefficient (NPS)": "0.27",
+    "Average Rating": "6.9",
+  },
+  {
+    "Subject": "Amount of communication",
+    "Ratings": "54",
+    "Priority Score": "6.112197150687439",
+    "ABS Correlation Coefficient (NPS)": "0.18",
+    "Average Rating": "7.0",
+  },
+  {
+    "Subject": "Quick and easy application form",
+    "Ratings": "104",
+    "Priority Score": "2.46613501750342",
+    "ABS Correlation Coefficient (NPS)": "0.12",
+    "Average Rating": "7.5",
+  },
+  {
+    "Subject": "Easy to find job online",
+    "Ratings": "103",
+    "Priority Score": "2.035884017394471",
+    "ABS Correlation Coefficient (NPS)": "0.15",
+    "Average Rating": "8.1",
+  },
+  {
+    "Subject": "Useful job advertisement",
+    "Ratings": "80",
+    "Priority Score": "-1.5785995416691625",
+    "ABS Correlation Coefficient (NPS)": "0.14",
+    "Average Rating": "7.2",
+  },
+];
+
+var divGroup = [
+    {
+        "class": "topLeft",
+        "transform": 'translate(0px, 0px)',
+        "color": "#f29696",
+        "text": "Improve"
+    }, {
+        "class": "topRight",
+        "transform": 'translate(478px, 0px)',
+        "color": "#bddaa5",
+        "text": "Leverage"
+    }, {
+        "class": "bottomLeft",
+        "transform": 'translate(0px, 196px)',
+        "color": "#f7e39c",
+        "text": "Monitor"
+    }, {
+        "class": "bottomRight",
+        "transform": 'translate(478px, 196px)',
+        "color": "#d5e8ff",
+        "text": "Maintain"
+    },
+];
+
+var colorMatcher = [
+    {
+        "color": data.map((d,i)=> {
+            if(d['ABS Correlation Coefficient (NPS)'] > 0.5 && d['Average Rating'] < 7.25) {
+                return "#f29696"
+            }
+            if(d['ABS Correlation Coefficient (NPS)'] < 0.5 && d['Average Rating'] < 7.25) {
+                return "#f7e39c"
+            }
+            if (d['ABS Correlation Coefficient (NPS)'] > 0.5 && d['Average Rating'] > 7.25) {
+                return "#bddaa5"
+            }
+            return "#d5e8ff"
+        })
+    }
+];
+
+const minX = d3.min(data, d => d['Average Rating'])
+const maxX = d3.max(data, d => d['Average Rating'])
+
+const minY = d3.min(data, d => d['ABS Correlation Coefficient (NPS)'])
+const maxY = d3.max(data, d => d['ABS Correlation Coefficient (NPS)'])
+
+//   Add X axis
+var x = d3.scaleLinear()
+    .domain([minX, maxX])
+    .range([ 0, width]);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+// Add Y axis
+var y = d3.scaleLinear()
+    .domain([0, 1])
+    .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+//colored boxes with text inside
+svg.append('g')
+    .selectAll("foreignObject")
+    .data(divGroup)
+    .enter()
+    .append("foreignObject")
+        .attr('class', function (d) {return d.class;})
+        .style('background-color', function (d) {return d.color})
+        .style('width', '50%')
+        .style('height', '196.25px')
+        .style("transform", function (d) {return d.transform})
+    .append("xhtml:body")
+    .html(d => d.text);
+
+var tooltip = d3.select("#chart")
+    .data(data)
+    .append("div")
+    .style("visibility", "hidden")
+    // .attr("class", "tooltip")
+    .style("background-color", 'white')
+    .style('border', '1px solid')
+    .style('border-radius', '5px')
+    .style('padding', '10px')
+    .style('position', 'absolute')
+    .style('z-index', '3')
+    .style("top", d => d['ABS Correlation Coefficient (NPS)'])
+    .style("bottom", d => d['Average Rating']);
+
+var mouseover = function(d) {
+    tooltip
+        .style("visibility", "visible")
+}
+
+var mousemove = function(d) {
+    tooltip
+      .html(d['Subject'] + "<br><br>Ratings: " + d['Ratings'] + "<br>Priority Score: " + d['Priority Score'] + "<br><br> ABS Correlation Coefficient (NPS) is: " + d['ABS Correlation Coefficient (NPS)'] + "<br> Average Rating is: " + d['Average Rating'])
+      .style("left", (d3.mouse(this)[0]+90) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+      .style("top", (d3.mouse(this)[1]) + "px")
+}
+
+var mouseleave = function(d) {
+    tooltip
+      .transition()
+      .duration(100)
+      .style("visibility", "hidden")
+}
+
+// Circles with text inside
+svg.append('g')
+    .selectAll("foreignObject")
+    .data(data.filter(function(d,i){return i<50})) // the .filter part is just to keep a few dots on the chart, not all of them
+    .enter()
+    .append("foreignObject")
+        .attr("x", d => { return x(d['Average Rating']); } )
+        .attr("y", d => { return y(d['ABS Correlation Coefficient (NPS)']); } )
+        // .attr('class', 'blot')
+        .style('border-radius', '100%')
+        .style('background-color', 'rgb(255, 255, 255)')
+        .style('border', '1px solid rgb(51, 51, 51)')
+        .style('width', '25px')
+        .style('height', '25px')
+        .style('cursor', 'default')
+        
+        // hover
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+    // add text inside the circle    
+    .append("xhtml:body")
+        .html((d, i) => i + 1)
+        .style('margin', 'auto')
+        .style('text-align', 'center')
+        .style('padding-top', "3px");
+
+// Ranking column
+var rankingId = d3.select("#vis").style('display', 'flex').append('div').attr('id', 'ranking').style('position', 'relative')
+.style('display', 'inline-blck')
+
+var circlesContainer = rankingId
+  .append("div")
+//   .attr("class", "circle-container")
+  .style('position', 'absolute')
+  .style('left', '-9px')
+  .style('top', '10px')
+
+colorMatcher[0].color.map((color, i) => {
+  circlesContainer
+    .append("div")
+    // .attr("class", "circle")
+    .style('width', '25px')
+    .style('height', '25px')
+    .style('border', '1px solid #333')
+    .style('border-radius', '100%')
+    .style('margin-bottom', '13px')
+    .style('text-align', 'center')
+    .style('display', 'flex')
+    .style('align-items', 'center')
+    .style('justify-content', 'center')
+    .style("background-color", color)
+    .html(i+1)
+});
+
+// rankingId.append('div').attr("class","ranking-line")
+
+var textContainer = rankingId
+  .append("div")
+//   .attr('class', 'text-container');
+  .style('margin-left', '30px')
+  .style('margin-top', '15px');
+
+data.map((data, i) => {
+    textContainer.append('p')
+    // .attr('class', 'text')
+    .style('margin-top', '0px')
+    .style('margin-bottom', '27px')
+    .style('line-height', '13.5px')
+    .text(data['Subject'])
+});
